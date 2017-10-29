bb48adcd3212ed35d352f4b046701adb9f23dfc9
xufeng1994 <914650562@qq.com>
2017-10-29 22:32:41 +0800

    update some function

--- common/ui/uiAction.js
+++ common/ui/uiAction.js
@@ -6,47 +6,41 @@ let loginPage = require('../../config/uiconfig/loginPage')
 let userinfo = require('../../config/userinfo.json')
 let app = require('../../config/app.config');
 let util = require('./util')
-let xufeng =userinfo.xufeng
+let xufeng = userinfo.xufeng
 
-let userLogin = async function(web,userName,passWord){
-    await web.get(app.baseUrl);
-    await web.findElement(indexpage.loginhref).click();
-    await web.findElement(loginPage.username).sendKeys(userName);
-    await web.findElement(loginPage.password).sendKeys(passWord);
-    await web.findElement(loginPage.submit).click();
-}
+    let userLogin = async function (web, userName, passWord) {
+        await web.get(app.baseUrl);
+        await web.findElement(indexpage.loginhref).click();
+        await web.findElement(loginPage.username).sendKeys(userName);
+        await web.findElement(loginPage.password).sendKeys(passWord);
+        await web.findElement(loginPage.submit).click();
+    }
 
-let userRegister = async function (web,loginname,pass,re_pass,email){
-    await web.get(app.baseUrl)
-    await web.findElement(registerPage.registerUrl).click();
-    await web.findElement(registerPage.loginName).sendKeys(loginname);
-    await web.findElement(registerPage.pass).sendKeys(pass);
-    await web.findElement(registerPage.rePass).sendKeys(re_pass);
-    await web.findElement(registerPage.email).sendKeys(email);
-    await web.findElement(registerPage.sudmit).click();
-}
+     let userRegister = async function (web, loginname, pass, re_pass, email) {
+        await web.get(app.baseUrl)
+        await web.findElement(registerPage.registerUrl).click();
+        await web.findElement(registerPage.loginName).sendKeys(loginname);
+        await web.findElement(registerPage.pass).sendKeys(pass);
+        await web.findElement(registerPage.rePass).sendKeys(re_pass);
+        await web.findElement(registerPage.email).sendKeys(email);
+        await web.findElement(registerPage.sudmit).click();
+    }
 
-let saveScreenShots = async function (web){
-    let screenshotdir = util.getScreenshotsDir();
-    let imagedata = await web.takeScreenshot();
-    fs.writeFileSync(screenshotdir+ "/"+ new Date().valueOf()+".png",imagedata,"base64");
-}
+    let saveScreenShots = async function (web) {
+        let screenshotdir = util.getScreenshotsDir();
+        let imagedata = await web.takeScreenshot();
+        fs.writeFileSync(screenshotdir + "/" + new Date().valueOf() + ".png", imagedata, "base64");
+    }
 
-let releaseTopic = async function(web,loginname,password){
-    await userLogin(web,loginname,password);
-    await web.findElement(indexpage.topicButton).click();
-    
-}
+    let releaseTopic = async function (web, loginname, password) {
+        await this.userLogin(web, loginname, password);
+        await web.findElement(indexpage.topicButton).click();
+
+    }
 
 
-
+module.exports.userAction = userAction;
+module.exports.releaseTopic = releaseTopic;
 module.exports.userLogin = userLogin;
 module.exports.userRegister = userRegister;
 module.exports.saveScreenShots = saveScreenShots;
-module.exports.releaseTopic = releaseTopic;
-
-
-
-
-
-
--- uiTest/userTest/topic.js
+++ uiTest/userTest/topic.js
@@ -1,7 +1,9 @@
 require("chromedriver")
 var webdriver = new require("selenium-webdriver")
+let until = require("selenium-webdriver/lib/until")
 let web = new webdriver.Builder().forBrowser("chrome").build()
-let uiAction = require("../../common/ui/uiAction")
+let {userAction} = require("../../common/ui/uiAction")
+let useraction = new userAction();
 let userinfo = require("../../config/userinfo")
 let xufeng = userinfo.xufeng
 let postPage = require("../../config/uiconfig/postPage")
@@ -17,22 +19,22 @@ describe("发布话题", async function(){
     it ("选择正常发布话题", async function(){
         // await uiAction.userRegister(web,xufeng.username,xufeng.password,xufeng.password,xufeng.email)
         // dbAction.activeUser(xufeng.username)
-        await uiAction.releaseTopic(web,xufeng.username,xufeng.password)
+        await useraction.releaseTopic(web,xufeng.username,xufeng.password)
         await web.findElement(postPage.select).click()
-        await web.findElement(postPage.select.ask).click()
-        await web.findElement(postPage.title).sendKeys("你在这里可以任意问答");
+        await web.findElement(postPage.select.share).click()
+        await web.findElement(postPage.title).sendKeys("关于JavaScript的问题回答都在这里");
         await web.findElement(postPage.text).click()
-        await web.actions().mouseMove(web.findElement(postPage.textArea)).sendKeys("可以在楼下回复如：你好啊，交个朋友吧").perform()
+        await web.actions().mouseMove(web.findElement(postPage.textArea)).sendKeys("可以在该楼层回复问答技术相关问题").perform()
         await web.findElement(postPage.link).click()
         await web.sleep(2000)
-        await web.findElement(postPage.link.title).sendKeys("优酷")
-        await web.findElement(postPage.link.linkurl).sendKeys("www.youku.com")
+        await web.findElement(postPage.link.title).sendKeys("youtube")
+        await web.findElement(postPage.link.linkurl).sendKeys("www.youtube.com")
         await web.findElement(postPage.link.submit).click()
         await web.sleep(1000)
         await web.findElement(postPage.image).click()
         await web.sleep(2000)
         await web.findElement(postPage.image.file).sendKeys(picpath+"/1508336193699.png")
-        
+        await web.sleep(2000)
         await web.findElement(postPage.submit).click()
         
     })
